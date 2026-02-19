d86aa77beb4f3758689ce47957c401c70e1c23b2
Anjali Purohit <purohitanjali098@gmail.com>
2026-02-19 14:34:38 +0530

    edit functionality

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './client/src/App.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
     <App />
